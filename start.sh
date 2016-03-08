2d6ea64daa9985486c4a00097ce260d764667c9d
contact@muthupandian.in <contact@muthupandian.in>
2016-03-08 18:03:47 -0500

    SH

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-forever stop api/api.js
+forever stop parser-server/cms_api/api.js
 forever stop parse-server/index.js
-forever start api/api.js
+forever start parse-server/cms_api/api.js
 forever start parse-server/index.js
 
