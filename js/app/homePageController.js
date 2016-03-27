c9f1489437473269f164fb0b61c6a2dc1854e6eb
Muthupandian <contact@muthupandian.in>
2016-03-27 19:42:22 +0530

    APP

--- js/app/homePageController.js
+++ js/app/homePageController.js
@@ -12,10 +12,23 @@ app.controller('homePageController', function($scope, $http, $location,
 		});
 	};
 
+	$scope.fetchUsers = function(area, category, page, from, max) {
+		var fetchArticleUrl = APIUrl + "/getSiteUsers/" + category + "/" + page
+				+ "/" + from + "/" + max;
+		$scope[area] = [];
+		$http.get(fetchArticleUrl).then(function(response) {
+			$.each(response.data, function(i, l) {
+				$scope[area].push(l);
+			});
+		});
+	};
+
 	$scope.fetchArticles("topNews", "any", 1, 1, 4, "all");
 	$scope.fetchArticles("bigPic1", "any", 1, 1, 1, "all");
 	$scope.fetchArticles("bigPic2", "any", 1, 1, 3, "all");
 	$scope.fetchArticles("categoryList1", "any", 1, 1, 4, "all");
 	$scope.fetchArticles("recentStories", "any", 1, 1, 20, "all");
+
+	$scope.fetchUsers("authors", "any", 1, 1, 20);
 
 });
