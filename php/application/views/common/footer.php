9cb656618dbc3be89dc54db99be85a69c6b157e4
Muthupandian <contact@muthupandian.in>
2016-05-05 00:15:28 +0530

    APP

--- php/application/views/common/footer.php
+++ php/application/views/common/footer.php
@@ -1,7 +1,7 @@
 <!-- CLOSING FIRST CONTAINER WHICH IS IN HEADER.PHP -->
 </div>
 
-<script src="<?php echo base_url();?>assets/js/jquery.min.js"></script>
-<script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
-<script src="<?php echo base_url();?>assets/js/jquery-ui.js"></script>
-<script src="<?php echo base_url();?>assets/js/localConfig.js"></script>
+<script src="<?php echo SITE_PATH;?>assets/js/jquery.min.js"></script>
+<script src="<?php echo SITE_PATH;?>assets/js/bootstrap.min.js"></script>
+<script src="<?php echo SITE_PATH;?>assets/js/jquery-ui.js"></script>
+<script src="<?php echo SITE_PATH;?>assets/js/localConfig.js"></script>
