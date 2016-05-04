38db2fa950a701da9370d74834b7d795316c6d31
Muthupandian <contact@muthupandian.in>
2016-05-05 00:16:22 +0530

    APP

--- php/application/views/common/rightPanel.php
+++ php/application/views/common/rightPanel.php
@@ -1,8 +1,8 @@
 ADS
-<img src="<?php echo base_url();?>ads/01.jpg" class="">
+<img src="<?php echo SITE_PATH;?>ads/01.jpg" class="">
 <hr>
-<img src="<?php echo base_url();?>ads/02.jpg" class="">
+<img src="<?php echo SITE_PATH;?>ads/02.jpg" class="">
 <hr>
-<img src="<?php echo base_url();?>ads/03.jpg" class="">
+<img src="<?php echo SITE_PATH;?>ads/03.jpg" class="">
 <hr>
-<img src="<?php echo base_url();?>ads/04.jpg" class="">
+<img src="<?php echo SITE_PATH;?>ads/04.jpg" class="">
