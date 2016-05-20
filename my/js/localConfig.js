eda0ef780be5493f41bcc6ebe35819b585fd98aa
Muthupandian <contact@muthupandian.in>
2016-05-20 22:47:28 +0530

    Git ignore

--- my/js/localConfig.js
+++
@@ -1,4 +0,0 @@
-//var APIUrl = "http://192.168.0.102:9991";
-//var APIUrl = "http://128.199.93.125:9991";
-var APIUrl = "http://192.168.1.139:9991";
-var PHP_API = "http://192.168.1.139/tc/my/phpApi";
