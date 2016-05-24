286953fce9b623046f203c291e08fb9752a9e740
Muthupandian <contact@muthupandian.in>
2016-05-25 02:59:12 +0530

    APP

--- php/application/views/singlePost.php
+++ php/application/views/singlePost.php
@@ -1,15 +1,6 @@
 <?php $this->load->view ( 'common/htmlBlocksSet2' );?>
 <hr>
-<div class="col-sm-12">
-	<div class="col-sm-3"
-		style="background-color:; border: 1px solid black; padding-left: 3px; padding-right: 2px;">
-		<div style="">
-		<?php
-		$featureArticles ['featuredArticleList'] = $allFeatureArticles;
-		$this->load->view ( 'common/widgets/featuredArticlesList', $featureArticles );
-		?>
-		</div>
-	</div>
+<div class="col-sm-12 well">
 	<div class="col-sm-9">
 		<h4><?php echo $article['title']?></h4>
 		<div class="" style="padding-bottom: 30px">
@@ -18,10 +9,12 @@
 				<?php echo IMAGE_PATH.$article['userItem']['profilePic'];?>
 				)"></div>
 			<div style="padding-top: 10px" class="col-sm-9">
-				<a href="<?php echo SITE_PATH.'author/'.$article['userApi'];?>"><?php echo $article['userItem']['name'];?></a>
-				<abbr class="timeago" title="<?php echo $article['createdAt'];?>"></abbr> 
-				<?php if(isset($article['categoryItemData']['title'])) echo ' in '.$article['categoryItemData']['title'];?><br>
-				<hr>
+				By: <a href="<?php echo SITE_PATH.'author/'.$article['userApi'];?>"><?php echo $article['userItem']['name'];?></a>
+				<abbr class="timeago" title="<?php echo $article['createdAt'];?>"></abbr>
+			
+				<?php if(isset($article['categoryItemData']['title'])) echo ' in <span class="well" style="background-color:orange;padding:3px;color:white;font-weight:bold">'.$article['categoryItemData']['title'];?></span><br>
+			</div>
+			<div style="padding-top: 10px" class="col-sm-12">
 				<?php if(isset($article['featureImageURL'])){?>
 					<img src="<?php echo IMAGE_PATH.$article['featureImageURL'];?>"
 					style="max-height: 300px">
@@ -29,6 +22,15 @@
 				
 				<p><?php echo $article['postDetail'];?></p>
 			</div>
+		</div>
+	</div>
+	<div class="col-sm-3"
+		style="background-color:; border: 1px solid black; padding-left: 3px; padding-right: 2px;">
+		<div style="">
+		<?php
+		$featureArticles ['featuredArticleList'] = $allFeatureArticles;
+		$this->load->view ( 'common/widgets/featuredArticlesList', $featureArticles );
+		?>
 		</div>
 	</div>
 </div>
