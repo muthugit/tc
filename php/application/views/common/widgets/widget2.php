1080cd5f28fa428acf83388c23572da3ca81f271
Muthupandian <contact@muthupandian.in>
2016-06-21 11:38:42 +0530

    APP

--- php/application/views/common/widgets/widget2.php
+++ php/application/views/common/widgets/widget2.php
@@ -6,7 +6,8 @@
 		<h4 style="color: <?php echo $titleColor;?>">
 			<b><?php echo $widgetCategoryTitle;?></b>
 		</h4>
-		<a  style="color: <?php echo $titleColor;?>" href="<?php echo SITE_PATH.$categoryUrl;?>">Read more</a>
+		<a  style="color: <?php echo $titleColor;?>" href="<?php echo SITE_PATH.$categoryUrl;?>">Read
+			more</a>
 
 	</div>
 	<?php
@@ -16,7 +17,13 @@
 			?>
 	<div class="col-sm-2">
 		<div class="col-sm-12" style="padding: 0px;">
+		<?php
+			if (isset ( $article ['featureImageURL'] )) {
+				?>
 			<img src="<?php echo IMAGE_PATH.$article['featureImageURL'];?>">
+			<?php }else{?>
+			<img src="http://www.padaippaligalulagam.com/logo.png">
+			<?php }?>
 		</div>
 		<div class="col-sm-12" style="padding: 0px;">
 			<h4>
