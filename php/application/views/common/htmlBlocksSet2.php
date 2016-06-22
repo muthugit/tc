21b4e4e0a3dd961b27417e80ac33b13e112482bd
Muthupandian <contact@muthupandian.in>
2016-06-23 02:22:43 +0530

    APP

--- php/application/views/common/htmlBlocksSet2.php
+++ php/application/views/common/htmlBlocksSet2.php
@@ -1,6 +1,6 @@
 <div class="container">
 	<div class="col-sm-12">
-		<div class="col-sm-2 col-xs-2">
+		<div class="col-sm-2 col-xs-6">
 	<?php
 	if (isSet ( $htmlContent7 ))
 		echo $htmlContent7 ['htmlContent'];
@@ -30,7 +30,7 @@
 		echo $htmlContent11 ['htmlContent'];
 	?>
 	</div>
-		<div class="col-sm-2">
+		<div class="col-sm-2 col-xs-6">
 	<?php
 	if (isSet ( $htmlContent12 ))
 		echo $htmlContent12 ['htmlContent'];
