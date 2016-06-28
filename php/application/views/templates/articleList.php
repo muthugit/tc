a519208a6e905854fe4430a69cc9b98e0658cf9a
Muthupandian <contact@muthupandian.in>
2016-06-29 03:28:12 +0530

    APP

--- php/application/views/templates/articleList.php
+++ php/application/views/templates/articleList.php
@@ -23,25 +23,28 @@ foreach ( $articleList as $article ) {
 	
 	$title = str_replace ( ',', '-', $article ['title'] );
 	$title = $title;
+	echo '<div class = "col-sm-12">';
 	
+	$featureImageUrl = SMALL_IMAGE_PATH . $article ['featureImageURL'];
+	if (isset ( $article ['featureImageURL'] ) && $article ['featureImageURL'] != '')
+		$isImageExist = true;
+		if ($isImageExist == true){?>
+			<div class = "col-sm-3" style="height:200px; overflow:hidden;background-size: cover;background-position: center;background-image:
+					url('<?php echo SMALL_IMAGE_PATH . $article ['featureImageURL'];?>')"></div>
+		<?php }
+	echo '<div class="col-sm-9  col-xs-12"  style="">';
 	echo '<h4><a href="' . SITE_PATH . 'post/show/' . $article ['objectId'] . '/' . urlencode ( $title ) . '">' . $article ['title'] . '</a></h4><br>';
-	echo '<div class="col-sm-12  col-xs-12"  style="padding-bottom: 20px">';
+	
 	echo '<div class="circleImage col-sm-3  col-xs-3" style="background-size: cover; background-image:
 				url(' . SMALL_IMAGE_PATH . $article ['userItem'] ['profilePic'] . ')"></div>';
 	echo '<div class="col-sm-9 col-xs-9" style="padding-bottom: 20px"><a href=' . SITE_PATH . 'author/' . $article ['userApi'] . '>' . ($article ['userItem'] ['name']) . '</a>
 					 @ <abbr class="timeago" title="' . $article ['createdAt'] . '"></abbr><br>
 					</div></div>';
 	$isImageExist = false;
-	$featureImageUrl = SMALL_IMAGE_PATH . $article ['featureImageURL'];
-	if (isset ( $article ['featureImageURL'] ) && $article ['featureImageURL'] != '')
-		$isImageExist = true;
-	if ($isImageExist == true){?>
-		<div class = "col-sm-12" style="width:200px; height:200px; overflow:hidden;background-size: cover;background-position: center;background-image:
-				url('<?php echo SMALL_IMAGE_PATH . $article ['featureImageURL'];?>')"></div>
-	<?php }
+	
 	if (isset ( $article ['description'] )) {
 		echo '<p class="well">' . $article ['description'] . '</p>';
 	}
-	echo '<hr>';
+	echo '</div><hr>';
 }
 ?>
